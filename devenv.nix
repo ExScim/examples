24cd2b5cad0ddac7cdb8baf948cea15452219928
Lars Oehnenamen <lars.oehnenamen@gmail.com>
2026-01-16 06:45:47 +0200

    chore: update devenv, add .gitignore, .envrc

--- devenv.nix
+++ devenv.nix
@@ -7,46 +7,20 @@
 }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  packages = [
+    pkgs.git
+    pkgs.inotify-tools
+    pkgs.tailwindcss-language-server
+    pkgs.tailwindcss_4
+    pkgs.nodejs
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.prettier
+  ];
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
   languages.elixir.enable = true;
+  languages.javascript.enable = true;
 
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
+  env.TAILWINDCSS_PATH = "${pkgs.lib.getExe pkgs.tailwindcss_4}";
 
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
+  git-hooks.hooks.mix-format.enable = true;
 }
